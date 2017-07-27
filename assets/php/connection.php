3189b69183d49280111670a7482e3ca45763003b
zjw93615 <zjw93615@163.com>
2017-07-27 17:39:14 +1000

    Add Linux Server connection Info

--- assets/php/connection.php
+++ assets/php/connection.php
@@ -1,5 +1,6 @@
 <?php
   # Database Connection Here...
-  $dbc = mysqli_connect('localhost:8889', 'jiaweiweb', 'LF5VRdpkY7MWz3hV', 'jiaweiweb') OR die('Error: '.mysqli_connect_error());
+  # $dbc = mysqli_connect('localhost:8889', 'jiaweiweb', 'LF5VRdpkY7MWz3hV', 'jiaweiweb') OR die('Error: '.mysqli_connect_error());
+  $dbc = mysqli_connect('localhost:8889', 'root', 'zjwzjw93615', 'jiaweiweb') OR die('Error: '.mysqli_connect_error());
 
 ?>
